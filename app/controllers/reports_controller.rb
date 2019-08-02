4cf12d3e3e54c715d88f5cc138f665b22f64b3b0
panjy888@gmail.com <Lestarikanselalu8>
2019-08-03 05:22:13 +0700

    tambahan rescue (error handling)

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,7 +10,12 @@ class ReportsController < ApplicationController
 		else
 			DaftarKursu.find_by_sql("select jenis_kursu_id, instruktur_id, jenis_kursus.tarif, jenis_kursus.lama_belajar
 				from daftar_kursus,murids,jenis_kursus
-				where daftar_kursus.murid_id=murids.id and daftar_kursus.jenis_kursu_id=jenis_kursus.id")
+				where daftar_kursus.murid_id=murids.id and daftar_kursus.jenis_kursu_id=jenis_kursus.id") rescue nil
 		end
+		rescue
+			if @daftar_kursu.nil?
+				flash[:danger] = 'Silakan Refresh terlebih dahulu'
+				redirect_to request.referrer
+			end
 	end
 end
